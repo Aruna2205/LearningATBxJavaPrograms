a2632352021f3612f514bfc24eecab857c61dc52
Aruna K <arunakolanji2000@gmail.com>
2025-03-12 20:53:01 +0530

    11 mar code commit

---
+++ src/ex_01_Java_Basic/NineTable.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basic;
+
+public class NineTable {
+    public static void main(String[] args) {
+        int n=9;
+        System.out.println("Result for Nineth Table ");
+        System.out.printf("9*1=%d%n",n*1);
+        System.out.printf("9*2=%d%n",n*2);
+        System.out.printf("9*3=%d%n",n*3);
+        System.out.printf("9*4=%d%n",n*4);
+        System.out.printf("9*5=%d%n",n*5);
+        System.out.printf("9*6=%d%n",n*6);
+        System.out.printf("9*7=%d%n",n*7);
+        System.out.printf("9*8=%d%n",n*8);
+        System.out.printf("9*9=%d%n",n*9);
+        System.out.printf("9*10=%d%n",n*10);
+    }
+}
